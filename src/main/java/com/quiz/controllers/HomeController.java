65a4495e5623a55d188385c1a53fc75f554fb23c
shankar Ghimire <ssghimire@gmail.com>
2021-05-13 23:01:14 -0400

    first commit

---
+++ src/main/java/com/quiz/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.quiz.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String goHome(){
+        return "index.html";
+    }
+
+}
